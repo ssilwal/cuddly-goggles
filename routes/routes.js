a5d30f4fda1de4f03954577d6998b1d21e200e03
cesaribarra10 <cfi2103@columbia.edu>
2017-04-17 03:27:38 -0400

    added admin, home, login pages and routes

--- routes/routes.js
+++ routes/routes.js
@@ -2,4 +2,12 @@ module.exports = (app) => {
 	app.get('/', function (req, res) {
 		res.render('index', {title : "Hello World"});
 	});
+
+	app.get('/home', (req, res) => {
+		res.render('home');
+	});
+
+	app.get('/admin', (req, res) => {
+		res.render('admin');
+	});
 };
